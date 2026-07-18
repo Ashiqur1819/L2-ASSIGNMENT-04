bd3722838e4f8989a08703c6f2ce7bcb85057903
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2026-07-18 19:49:22 +0600

    Remove duplication from category controller and service file

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -4,17 +4,6 @@ import { categoryService } from "./category.service";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 
-const createCategory = catchAsync(async (req: Request, res: Response) => {
-  const result = await categoryService.createCategory(req.body);
-
-  sendResponse(res, {
-    statusCode: httpStatus.CREATED,
-    success: true,
-    message: "Category created successfully",
-    data: result,
-  });
-});
-
 const getAllCategories = catchAsync(async (req: Request, res: Response) => {
   const result = await categoryService.getAllCategories();
 
@@ -39,41 +28,7 @@ const getSingleCategory = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-const updateCategory = catchAsync(async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  const result = await categoryService.updateCategory(id as string, req.body);
-
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Category updated successfully",
-    data: result,
-  });
-});
-
-const deleteCategory = catchAsync(async (req: Request, res: Response) => {
-  const { id } = req.params;
-
-  await categoryService.deleteCategory(id as string);
-
-  res.status(httpStatus.OK).json({
-    success: true,
-    message: "Category deleted successfully",
-  });
-
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Category deleted successfully",
-    data: null,
-  });
-});
-
 export const categoryController = {
-  createCategory,
   getAllCategories,
   getSingleCategory,
-  updateCategory,
-  deleteCategory,
 };
